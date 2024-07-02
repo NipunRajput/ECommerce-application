55071ecde387966a06f8aae06d9235d501c8c14b
Nipun <nipun.rajput.21cse@bmu.edu.in>
2024-07-03 00:49:03 +0530

    Updated with Middlewares

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,7 +5,8 @@
 
 const userModel = require('../models/user.models');
 const authController=require('../controller/auth.controller')
+const authMW=require("../middlewares/auth.mw")
 
 module.exports=(app)=>{
-    app.post("/ecomm/api/v1/auth/signup",authController.signup)
+    app.post("/ecomm/api/v1/auth/signup",[authMW.verifysignupBody],authController.signup)
 }
